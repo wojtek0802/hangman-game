969367bc98624af39579358a570c0b5ae52ad695
wojtek0802 <37045445+wojtek0802@users.noreply.github.com>
2019-03-16 19:02:59 +0100

    Add files via upload

--- wisielec.js
+++ wisielec.js
@@ -1,20 +1,23 @@
 var letters = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŹŻ"
-var solutions=['czas to pieniądz', 'baba z wozu koniom lżej', 'cel uświęca środki'];
+var przyslowia=["czas leczy rany", "baba z wozu koniom lżej","bez pracy nie ma kołaczy", "co nagle to po diable", "fortuna kołem się toczy"];
+var informatyka=["programowanie", "system operacyjny", "baza danych", "klawiatura"];
 var pickedSolution ="";
 var hiddenSolution="";
 var mistakes = -1;
+var currentCategory = przyslowia;
 
 function start()
 {   
-    getRandomSolution();
-    var divContent = "";
-    for (var i=0; i<letters.length; i++)
-        {
-            divContent += '<div class="letter" id="l' + i + '" onClick="check('+i+')">' + letters.charAt(i) +'</div>';
-        }
-    var element = document.getElementById('alfabet');
-    element.innerHTML = divContent;
+    currentCategory = przyslowia;
+    getRandomSolution(currentCategory);
+    updateSolution();
+    generateAlfabet();
+    setHoverAtribiuttes();
     
+}
+
+function setHoverAtribiuttes() //funkcja ustawia w javaScripcie co ma się stać po najechaniu na litery myszką
+{
     var letterDivs = document.getElementsByClassName("letter"); 
     for(i=0; i<letterDivs.length; i++)
         {
@@ -36,12 +39,12 @@ function start()
         }
 }
 
-function getRandomSolution()
+function getRandomSolution(category)
 {
-    var solutionDiv = document.getElementById('solution');
-    var index = Math.floor(Math.random() * solutions.length); //losowanie indeksu hasła z tablicy solutions
-    pickedSolution = solutions[index];
-
+    
+    var index = Math.floor(Math.random() * category.length); //losowanie indeksu hasła z tablicy solutions  
+    pickedSolution = category[index];
+    hiddenSolution="";
     for(i=0; i<pickedSolution.length; i++)
         {
             if(pickedSolution.charAt(i) == ' ')
@@ -50,13 +53,36 @@ function getRandomSolution()
                 hiddenSolution += '-';
         }
 
-    solutionDiv.innerHTML = "Odgadnij przysłowię...<br/>" + hiddenSolution;
+    updateSolution();
+    generateAlfabet();
 }
 
 function updateSolution()
 {
+    var chosenCategory = "";
+    switch(currentCategory)
+        {
+            case przyslowia:
+                chosenCategory = "PRZYSŁOWIA";
+                break;
+            case informatyka:
+                chosenCategory = "INFORMATYKA"
+                break;
+            default: break;
+        }
     var solutionDiv = document.getElementById('solution');
-    solutionDiv.innerHTML = "Odgadnij przysłowię...<br/>" + hiddenSolution;
+    solutionDiv.innerHTML = "Kategoria: "+ chosenCategory + "<br/>"+ hiddenSolution;
+}
+
+function generateAlfabet()
+{
+    var divContent = "";
+    for (var i=0; i<letters.length; i++)
+        {
+            divContent += '<div class="letter" id="l' + i + '" onClick="check('+i+')">' + letters.charAt(i) +'</div>';
+        }
+    var element = document.getElementById('alfabet');
+    element.innerHTML = divContent;   
 }
 
 function check(nr)
@@ -69,7 +95,7 @@ function check(nr)
                 {
                     isInSolution = true;
                     var okSound = new Audio("pop.wav");
-                    okSound.play();
+                    okSound.play(); 
                     var letterDiv = document.getElementById("l" + nr);
                 
                     letterDiv.style.backgroundColor = ("green");
@@ -124,22 +150,46 @@ function reveal(nr, newLetter)
 function playerLost()
 {
     var defeat = 'Przegrałeś! Poprawna odpowiedź to: "' + pickedSolution + '" <br/>';
-    var resetButton = '<input type="button" id="resetButton" onclick="resetGame()" value="SPRÓBUJ JESZCZE RAZ"/>'
+    var resetButton = '<input type="button" class="myButton" onclick="resetGame(currentCategory)" value="SPRÓBUJ JESZCZE RAZ"/>'
     var solutionDiv = document.getElementById('solution');
-    solutionDiv.innerHTML = defeat + resetButton;
+    solutionDiv.innerHTML = defeat;
+    document.getElementById('buttons').innerHTML += resetButton;
 }
 
 function playerWon()
 {
     var victory = 'Gratulację! Udało Ci się wygrać!'
-    var resetButton = '<input type="button" id="resetButton" onclick="resetGame()" value="ZAGRAJ JESZCZE RAZ"/>'
+    var resetButton = '<input type="button" class="myButton" onclick="resetGame(currentCategory)" value="ZAGRAJ JESZCZE RAZ"/>'
     var solutionDiv = document.getElementById('solution');
-    solutionDiv.innerHTML = victory + resetButton;
+    solutionDiv.innerHTML = victory;
+    document.getElementById('buttons').innerHTML += resetButton;
 }
 
-function resetGame()
+function resetGame(category)
 {
-    location.reload();
+    pickedSolution = "";
+    hiddenSolution = "";
+    mistakes = -1;
+    var hangmanDiv = document.getElementById("hangman");
+    hangmanDiv.innerHTML = '<img src="" height="500">';
+    getRandomSolution(category);
+    setHoverAtribiuttes();
+    var buttonArea = document.getElementById("buttons");
+    buttonArea.innerHTML = '<input type="button" class="myButton" onclick="changeCategory()" value="ZMIEŃ KATEGORIE"/>';
+    
+}
+
+function changeCategory()
+{
+    var listArea = document.getElementById("buttons");
+    listArea.innerHTML = '<input type="button" class="myButton" onclick="chooseCategory(informatyka)" value="INFORMATYKA"/>';
+    listArea.innerHTML += '<input type="button" class="myButton" onclick="chooseCategory(przyslowia)" value="PRZYSŁOWIA"/>';
+}
+
+function chooseCategory(category)
+{
+    currentCategory = category;
+    resetGame(category);
 }
 
 window.onload = start;
